bbee4071552f24547ca9575f795d2b6960ed8853
KwangtaekJung <kt79.jung@gmail.com>
2022-04-17 18:44:29 +0900

    빌터 패턴 2부: 패턴적용하기(Director 적용)

--- src/main/java/com/example/inflearndesignpattern/_01_creational_patterns/_04_builder/_02_after/App.java
+++ src/main/java/com/example/inflearndesignpattern/_01_creational_patterns/_04_builder/_02_after/App.java
@@ -2,25 +2,15 @@ package com.example.inflearndesignpattern._01_creational_patterns._04_builder._0
 
 import com.example.inflearndesignpattern._01_creational_patterns._04_builder._01_before.TourPlan;
 
-import java.time.LocalDate;
-
 public class App {
 
     public static void main(String[] args) {
-        DefaultTourBuilder builder = new DefaultTourBuilder();
-        TourPlan plan = builder.title("칸쿤 여행")
-                .nightsAndDays(2, 3)
-                .startDate(LocalDate.of(2020, 12, 9))
-                .whereToStay("리조트")
-                .addPlan(0, "체크인하고 짐 풀기")
-                .addPlan(0, "저녁 식사")
-                .getPlan();
-        System.out.println("plan = " + plan);
 
+        TourDirector tourDirector = new TourDirector(new DefaultTourBuilder());
+        TourPlan tourPlan = tourDirector.cancunTrip();
+        System.out.println("tourPlan = " + tourPlan);
 
-        TourPlan longBeachTrip = builder.title("롱 비치")
-                .startDate(LocalDate.of(2021, 7, 15))
-                .getPlan();
+        TourPlan longBeachTrip = tourDirector.longBeachTrip();
         System.out.println("longBeachTrip = " + longBeachTrip);
     }
 }
