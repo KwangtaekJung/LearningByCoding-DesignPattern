bc3324a68f3b12e7772981563d56431fb022b7b1
KwangtaekJung <kt79.jung@gmail.com>
2022-04-19 22:29:34 +0900

    브릿지 패턴2부: 패턴적용하기(after)

---
+++ src/main/java/com/example/inflearndesignpattern/_02_structural_patterns/_07_bridge/_02_after/아킬리.java
@@ -0,0 +1,7 @@
+package com.example.inflearndesignpattern._02_structural_patterns._07_bridge._02_after;
+
+public class 아킬리 extends DefaultChampion {
+    public 아킬리(Skin skin) {
+        super(skin, "아킬리");
+    }
+}
