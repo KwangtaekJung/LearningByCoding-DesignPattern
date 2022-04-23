7edb5eead791abb57518f996bfa390f420e0f71b
KwangtaekJung <kt79.jung@gmail.com>
2022-04-23 20:49:05 +0900

    컴포짓 패턴 3부: 장점과 단점

--- src/main/java/com/example/inflearndesignpattern/_02_structural_patterns/_08_composite/_02_after/Client.java
+++ src/main/java/com/example/inflearndesignpattern/_02_structural_patterns/_08_composite/_02_after/Client.java
@@ -15,6 +15,8 @@ public class Client {
         client.printPrice(bag); //가방 안의 모든 아이템 가격들의 합
     }
 
+    //컴포짓 패턴의 장점
+    // - 새로운 종류의 Component 타입이 생기더라도 Client 코드(printPrice)도 변경되지 않는다.
     private void printPrice(Component component) {
         System.out.println(component.getPrice());
     }
