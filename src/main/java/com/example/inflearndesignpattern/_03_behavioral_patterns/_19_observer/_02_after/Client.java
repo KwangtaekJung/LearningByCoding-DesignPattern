1c13460d1d98042389f45d81e8141412bc9da451
KwangtaekJung <kt79.jung@gmail.com>
2022-05-10 15:50:10 +0900

    옵저버 패턴 2부: 패턴 적용하기

---
+++ src/main/java/com/example/inflearndesignpattern/_03_behavioral_patterns/_19_observer/_02_after/Client.java
@@ -0,0 +1,16 @@
+package com.example.inflearndesignpattern._03_behavioral_patterns._19_observer._02_after;
+
+public class Client {
+    public static void main(String[] args) {
+        ChatServer chatServer = new ChatServer();
+        User user1 = new User("keesun");
+        User user2 = new User("whiteship");
+
+        chatServer.register("오징어게임", user1);
+        chatServer.register("오징어게임", user2);
+
+        chatServer.register("디자인패턴", user1);
+
+        chatServer.sendMessage(user1, "오징어게임", "아.. 이름이 기억났어. 일남이야.. 오일남");
+    }
+}
