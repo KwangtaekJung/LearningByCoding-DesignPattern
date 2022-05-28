de88960324d65a92ade8194845505377b7f21c71
KwangtaekJung <kt79.jung@gmail.com>
2022-05-28 20:01:34 +0900

    템플릿 메소드 패턴 4부: 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/com/example/inflearndesignpattern/_03_behavioral_patterns/_022_template/_04_java/TemplateInJava.java
@@ -0,0 +1,10 @@
+package com.example.inflearndesignpattern._03_behavioral_patterns._022_template._04_java;
+
+import javax.servlet.http.HttpServlet;
+
+public class TemplateInJava {
+
+    public static void main(String[] args) {
+        HttpServlet httpServlet;
+    }
+}
